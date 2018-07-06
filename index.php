ea9d79ec21b24095d0e1d3c913e45f60c8842814
peps1840 <prachi.jain@technonjr.org>
2018-07-06 11:15:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<title>Resume</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" />
+<link rel="stylesheet" type="text/css" href="resume.css" media="all" />
+</head>
+<body>
+<div id="doc2" class="yui-t7">
+  <div id="inner">
+    <div id="hd">
+      <div class="yui-gc">
+        <div class="yui-u first">
+          <h1>Prachi Jain</h1>
+          <h2>Software Engineer, Student</h2>
+        </div>
+        <div class="yui-u">
+          <div class="contact-info">
+            <h3>prachitruejain@gmail.com</h3>
+            <h3>8290745400</h3>
+            
+          </div>
+          <!--// .contact-info -->
+        </div>
+      </div>
+      <!--// .yui-gc -->
+    </div>
+    <!--// hd -->
+    <div id="bd">
+      <div id="yui-main">
+        <div class="yui-b">
+          <div class="yui-gf">
+            <div class="yui-u first">
+              <h2>Profile</h2>
+            </div>
+            <div class="yui-u">
+              <p class="enlarge"> Lives in Udaipur also known as city of lakes.<br/> Date of birth - 25th november 1997.<br/> Linkedin profile - <a href="https://www.linkedin.com/in/prachi-jain-012662167/">https://www.linkedin.com/in/prachi-jain-012662167/</a> Github account - <a href="https://github.com/peps1840">https://github.com/peps1840 </a> </p>
+            </div>
+          </div>
+          <!--// .yui-gf -->
+          <div class="yui-gf">
+            <div class="yui-u first">
+              <h2>Skills</h2>
+            </div>
+           <div class="yui-u">
+              <div class="talent">
+                <h2>Web Design</h2>
+                <p>Have good knowledge in web application development. </p>
+              </div>
+              <div class="talent">
+                <h2>Application developmemt and deployment</h2>
+                <p> Developed applications on IBM cloud and also deployed on that.</p>
+              </div>
+              <div class="talent">
+                <h2>Software Developer</h2>
+                <p>Worked on java and c-sharp for the software development.</p>
+              </div>
+            </div>
+          </div>
+          <!--// .yui-gf -->
+          <div class="yui-gf">
+            <div class="yui-u first">
+              <h2>Technical</h2>
+            </div>
+            <div class="yui-u">
+              <ul class="talent">
+                <li>PHP</li>
+                <li>Java</li>
+                <li class="last">Javascript</li>
+              </ul>
+              <ul class="talent">
+                <li>Python</li>
+                <li>Angular JS</li>
+                <li class="last">HTML5, CSS3</li>
+              </ul>
+              <ul class="talent">
+                <li>OS X</li>
+                <li>Windows XP,7,10</li>
+                <li class="last">Linux</li>
+              </ul>
+            </div>
+          </div>
+          <!--// .yui-gf-->
+          <div class="yui-gf">
+            <div class="yui-u first">
+              <h2>Projects</h2>
+            </div>
+            <!--// .yui-u -->
+            <div class="yui-u">
+              <div class="job">
+                <h2>College Management</h2>
+                <h3>Language PHP</h3>
+                <h4><a href="https://github.com/peps1840/collegemanagement">https://github.com/peps1840/collegemanagement</a></h4>
+                <p> This project is the complete solution for any college. With a single web application all the basic functioning of college like registration,sharing notices ,assignment etc can be done. </p>
+              </div>
+              <div class="job">
+                <h2>GST Software</h2>
+                <h3> Using C#</h3>
+                <h4><a href="https://github.com/peps1840/github">https://github.com/peps1840/github</a></h4>
+                <p> This project is for the retails to maintain the bills and accounting work. </p>
+              </div>
+              <div class="job">
+                <h2>Chatbot</h2>
+                <h3>Using Watson Assistant</h3>
+                <h4><a href="https://insurancepro.mybluemix.net/">https://insurancepro.mybluemix.net/</a></h4>
+                <p> Chatbot for insurance company. Deployed on IBM Cloud </p>
+              </div>
+              
+            </div>
+            <!--// .yui-u -->
+          </div>
+                
+          <!--// .yui-gf -->
+          <div class="yui-gf last">
+            <div class="yui-u first">
+              <h2>Education</h2>
+            </div>
+            <div class="yui-u">
+              <h2>The Scholars Arena Senior Secondary School, Udaipur</h2>
+			  <h3>CBSE Boards (2001-2015)</h3>
+              <h2>Techno India NJR Institute of Technology, Udaipur</h2>
+			  <h3>Btech - Computer Science (2016-2019)</h3>
+            </div>
+          </div>
+          <!--// .yui-gf -->
+        </div>
+        <!--// .yui-b -->
+      </div>
+      <!--// yui-main -->
+    </div>
+    <!--// bd -->
+     <div id="ft">
+      <p>Prachi Jain &mdash; <a href="#">resume1.mybluemix.net</a> &mdash; 8290745400</p>
+      
+    </div>
+    <!--// footer -->
+  </div>
+  <!-- // inner -->
+</div>
+<!--// doc -->
+<div align=center>This template  downloaded form <a href='http://all-free-download.com/free-website-templates/'>free website templates</a></div></body>
+</html>
